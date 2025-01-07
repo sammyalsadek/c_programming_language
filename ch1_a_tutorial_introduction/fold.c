95b23261ad93fd15949f47b237aef084ad269551
Sammy Alsadek <awsammy@amazon.com>
2025-01-07 10:43:30 -0800

    update: added more code practice examples.

--- ch1_a_tutorial_introduction/fold.c
+++ ch1_a_tutorial_introduction/fold.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_LINES 1000
 #define MAX_LINE_LENGTH 20
+#define MAX_LINE_LENGTH_TMP 10000
 
 void get_lines(char lines[][MAX_LINE_LENGTH], unsigned short int *line_count);
+void copy_substring(char destination[], char source[], unsigned short int start_index, unsigned short int end_index);
 
 int main()
 {
@@ -23,28 +26,48 @@ int main()
 void get_lines(char lines[][MAX_LINE_LENGTH], unsigned short int *line_count)
 {
 	char input_character;
-	char temp_lines[];
-	unsigned short int character_count = 0;
-	unsigned short int last_blank_space_index = 0;
-	unsigned short int not_in_space = 0;
+	char temp_line[MAX_LINE_LENGTH_TMP];
+	unsigned short int temp_line_count = 0;
+	unsigned short int start_index = 0;
+	unsigned short int end_index = 0;
 
 	while((input_character = getchar()) != '\n')
 	{
-		if(character_count == MAX_LINE_LENGTH - 1)
+		temp_line[temp_line_count++] = input_character;
+	}
+	temp_line[temp_line_count] = '\n';
+
+	for(int i=0; i<temp_line_count; i++)
+	{
+		if(temp_line[i] == ' ' || temp_line[i] == '\t')
 		{
-			lines[*line_count][character_count] = '\0';
-			(*line_count)++;
-			character_count = 0;
+			end_index = i;
 		}
-		if(input_character == ' ' || input_character == '\t') && not_in_space)
+
+		if(((i-start_index)==(MAX_LINE_LENGTH-1)) && (end_index == start_index))
 		{
-			last_blank_space_index = character_count;
-			not_in_space = 1;
+			end_index = i;
+			copy_substring(lines[(*line_count)++], temp_line, start_index, end_index);
+			start_index = end_index;
 		}
-		else
+		else if(((i-start_index)==(MAX_LINE_LENGTH-1)) && (end_index != start_index))
 		{
-			lines[*line_count][character_count++] = input_character;
+			copy_substring(lines[(*line_count)++], temp_line, start_index, end_index);
+			start_index = end_index;
 		}
 	}
-	if(character_count > 0) (*line_count)++;
+
+	if(temp_line_count != start_index)
+	{
+		copy_substring(lines[(*line_count)++], temp_line, start_index, temp_line_count);
+	}
+}
+
+void copy_substring(char destination[], char source[], unsigned short int start_index, unsigned short int end_index)
+{
+	for(int i=0; i<(end_index-start_index); i++)
+	{
+		destination[i] = source[start_index+i];
+	}
+	destination[end_index-start_index] = '\0';
 }
